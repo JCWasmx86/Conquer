345944f3dfa53a3cbdd9ad4ecdcc70ce7c76e541
JCWasmx86 <JCWasmx86@t-online.de>
2020-12-05 16:33:13 +0100

    Refactoring

--- ConquerFrontend/src/org/jel/gui/ResourceButton.java
+++ ConquerFrontend/src/org/jel/gui/ResourceButton.java
@@ -36,26 +36,19 @@ final class ResourceButton extends JPanel {
 		this.city = city;
 		this.resource = resource;
 		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		final var costsForUpgrade = Shared.costs(
-				this.city.getLevels().get(this.resource == null ? Resource.values().length : this.resource.getIndex())
-						+ 1);
-		final var level = this.city.getLevels()
-				.get(resource == null ? Resource.values().length : this.resource.getIndex());
-		this.infoLabel = new JLabel((resource == null ? "Defense" : resource.getName()) + " Level " + level,
+		this.infoLabel = new JLabel(getInfoLabelText(),
 				new ImageResource(resource == null ? "defenseUpgrade.png" : resource.getImage()), SwingConstants.LEFT);
 		this.add(this.infoLabel);
 		this.upgradeThisResource = new JButton();
-		this.upgradeThisResource
-				.setText("Upgrade to level " + (level + 1) + ": " + String.format("%.2f", costsForUpgrade) + "coins");
+		this.upgradeThisResource.setText(getUpgradeThisResourceText());
 		this.upgradeThisResource.addActionListener(e -> {
 			if (resource != null) {
-				city.getGame().upgradeResource((byte) 0, resource, city);
+				city.getGame().upgradeResource((byte) Shared.PLAYER_CLAN, resource, city);
 			} else {
-				city.getGame().upgradeDefense((byte) 0, city);
+				city.getGame().upgradeDefense((byte) Shared.PLAYER_CLAN, city);
 			}
-			final var costsForUpgrade1 = Shared
-					.costs(city.getLevels().get(resource == null ? Resource.values().length : resource.getIndex()) + 1);
-			final var level1 = city.getLevels().get(resource == null ? Resource.values().length : resource.getIndex());
+			final var costsForUpgrade1 = Shared.costs(city.getLevels().get(getIndex()) + 1);
+			final var level1 = city.getLevels().get(getIndex());
 			ResourceButton.this.upgradeThisResource.setText(
 					"Upgrade to level " + (level1 + 1) + ": " + String.format("%.2f", costsForUpgrade1) + "coins");
 			cip.doUpdate();
@@ -64,38 +57,29 @@ final class ResourceButton extends JPanel {
 
 		this.maximumUpgrade = new JButton();
 		this.maximumUpgrade.setIcon(new ImageResource("max.png"));
-		double currentCoins = city.getGame().getCoins().get(0);
-		int currentLevel = level;
-		while (true) {
-			currentCoins -= Shared.costs(currentLevel + 1);
-			if (currentCoins <= 0) {
-				currentCoins += Shared.costs(currentLevel + 1);
-				break;
-			}
-			currentLevel++;
-		}
-		this.maximumUpgrade.setText("Maximum upgrade to level " + currentLevel + " for "
-				+ String.format("%.2f", city.getGame().getCoins().get(0) - currentCoins) + "coins");
+		this.maximumUpgrade.setText(getMaxUpgradeText());
 		this.add(this.maximumUpgrade);
 		this.maximumUpgrade.addActionListener(e -> {
 			if (resource != null) {
-				city.getGame().upgradeResourceFully((byte) 0, resource, city);
+				city.getGame().upgradeResourceFully((byte) Shared.PLAYER_CLAN, resource, city);
 			} else {
-				city.getGame().upgradeDefenseFully((byte) 0, city);
+				city.getGame().upgradeDefenseFully((byte) Shared.PLAYER_CLAN, city);
 			}
-			final var level1 = city.getLevels().get(resource == null ? Resource.values().length : resource.getIndex());
-			double currentCoins1 = city.getGame().getCoins().get(0);
+			final var level1 = city.getLevels().get(getIndex());
+			double currentCoins1 = city.getGame().getCoins().get(Shared.PLAYER_CLAN);
 			int currentLevel1 = level1;
 			while (true) {
-				currentCoins1 -= Shared.costs(currentLevel1 + 1);
+				var costs = Shared.costs(currentLevel1 + 1);
+				currentCoins1 -= costs;
 				if (currentCoins1 <= 0) {
-					currentCoins1 += Shared.costs(currentLevel1 + 1);
+					currentCoins1 += costs;
 					break;
 				}
 				currentLevel1++;
 			}
 			ResourceButton.this.maximumUpgrade.setText("Maximum upgrade to level " + currentLevel1 + " for "
-					+ String.format("%.2f", city.getGame().getCoins().get(0) - currentCoins1) + "coins");
+					+ String.format("%.2f", city.getGame().getCoins().get(Shared.PLAYER_CLAN) - currentCoins1)
+					+ "coins");
 			cip.doUpdate();
 		});
 		this.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -105,30 +89,59 @@ final class ResourceButton extends JPanel {
 		this.setVisible(true);
 	}
 
+	private int getIndex() {
+		return this.resource == null ? Resource.values().length : this.resource.getIndex();
+	}
+
+	private String getInfoLabelText() {
+		final var level = this.city.getLevels().get(getIndex());
+		return (this.resource == null ? "Defense" : this.resource.getName()) + " Level " + level;
+	}
+
+	private String getUpgradeThisResourceText() {
+		final var level = this.city.getLevels().get(getIndex());
+		final var costsForUpgrade = Shared.costs(
+				this.city.getLevels().get(this.resource == null ? Resource.values().length : this.resource.getIndex())
+						+ 1);
+		return "Upgrade to level " + (level + 1) + ": " + String.format("%.2f", costsForUpgrade) + "coins";
+	}
+
+	private String getMaxUpgradeText() {
+		final var level = this.city.getLevels().get(getIndex());
+		double currentCoins = city.getGame().getCoins().get(Shared.PLAYER_CLAN);
+		int currentLevel = level;
+		while (true) {
+			var costs = Shared.costs(currentLevel + 1);
+			currentCoins -= costs;
+			if (currentCoins <= 0) {
+				currentCoins += costs;
+				break;
+			}
+			currentLevel++;
+		}
+		return "Maximum upgrade to level " + currentLevel + " for "
+				+ String.format("%.2f", city.getGame().getCoins().get(Shared.PLAYER_CLAN) - currentCoins) + "coins";
+	}
+
 	/**
 	 * Updates all components of this component.
 	 */
 	void doUpdate() {
-		final var level = this.city.getLevels()
-				.get(this.resource == null ? Resource.values().length : this.resource.getIndex());
+		final var level = this.city.getLevels().get(getIndex());
 		if (this.city.getClan() == Shared.PLAYER_CLAN) {
 			if (level < 1000) {
-				this.infoLabel
-						.setText((this.resource == null ? "Defense" : this.resource.getName()) + " Level " + level);
+				this.infoLabel.setText(getInfoLabelText());
 			} else {
 				this.infoLabel.setText("Maximum value reached!");
 			}
 		} else {
 			this.infoLabel.setText((this.resource == null ? "Defense" : this.resource.getName()) + " Level ???");
 		}
-		final var costsForUpgrade = Shared.costs(
-				this.city.getLevels().get(this.resource == null ? Resource.values().length : this.resource.getIndex())
-						+ 1);
+		final var costsForUpgrade = Shared.costs(this.city.getLevels().get(getIndex()) + 1);
 		double currentCoins = this.city.getGame().getCoins().get(0);
-		if ((costsForUpgrade < currentCoins) && (this.city.getClan() == 0)) {
+		if ((costsForUpgrade < currentCoins) && (this.city.getClan() == Shared.PLAYER_CLAN)) {
 			this.upgradeThisResource.setEnabled(true);
-			this.upgradeThisResource.setText(
-					"Upgrade to level " + (level + 1) + ": " + String.format("%.2f", costsForUpgrade) + "coins");
+			this.upgradeThisResource.setText(getUpgradeThisResourceText());
 		} else if ((this.city.getClan() == 0) && (level == 1000)) {
 			this.upgradeThisResource.setEnabled(false);
 			this.upgradeThisResource.setText("Maximum value reached!");
@@ -138,17 +151,16 @@ final class ResourceButton extends JPanel {
 		}
 		int currentLevel = level;
 		while (true) {
-			currentCoins -= Shared.costs(currentLevel + 1);
+			var costs = Shared.costs(currentLevel + 1);
+			currentCoins -= costs;
 			if (currentCoins <= 0) {
-				currentCoins += Shared.costs(currentLevel + 1);
 				break;
 			}
 			currentLevel++;
 		}
 		if ((currentLevel != level) && (this.city.getClan() == Shared.PLAYER_CLAN)) {
 			this.maximumUpgrade.setEnabled(true);
-			this.maximumUpgrade.setText("Maximum upgrade to level " + currentLevel + " for "
-					+ String.format("%.2f", this.city.getGame().getCoins().get(0) - currentCoins) + "coins");
+			this.maximumUpgrade.setText(getMaxUpgradeText());
 		} else if ((this.city.getClan() == 0) && (level == 1000)) {
 			this.maximumUpgrade.setEnabled(false);
 			this.maximumUpgrade.setText("Maximum value reached!");
