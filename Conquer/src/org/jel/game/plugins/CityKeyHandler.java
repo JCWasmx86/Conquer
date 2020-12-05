7e80ff3ab65788899fcc075922f36e8f4a5f34e1
JCWasmx86 <JCWasmx86@t-online.de>
2020-12-05 14:55:18 +0100

    Documentation

--- Conquer/src/org/jel/game/plugins/CityKeyHandler.java
+++ Conquer/src/org/jel/game/plugins/CityKeyHandler.java
@@ -2,7 +2,17 @@ package org.jel.game.plugins;
 
 import org.jel.game.data.City;
 
+/**
+ * A callback that is called as soon as a city is focused and the specified key
+ * is pressed.
+ */
 @FunctionalInterface
 public interface CityKeyHandler {
-	void handle(String key, City c);
+	/**
+	 * Called as soon as a city is focused and the specified key is pressed
+	 * 
+	 * @param key  The key that was pressed
+	 * @param city The focused city.
+	 */
+	void handle(String key, City city);
 }
--- Conquer/src/org/jel/game/plugins/MessageListener.java
+++ Conquer/src/org/jel/game/plugins/MessageListener.java
@@ -2,8 +2,21 @@ package org.jel.game.plugins;
 
 import org.jel.game.messages.Message;
 
+/**
+ * A listener that reacts to different events of the EventList
+ */
 public interface MessageListener {
-	void added(Message s);
+	/**
+	 * Called when a message has been added to the list
+	 * 
+	 * @param message The added message
+	 */
+	void added(Message message);
 
-	void removed(Message s);
+	/**
+	 * Called when a message has been removed from the list
+	 * 
+	 * @param message The removed message
+	 */
+	void removed(Message message);
 }
