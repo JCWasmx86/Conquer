ac42e6dae9d6aa8451e34e88645913dc17bfa972
JCWasmx86 <JCWasmx86@t-online.de>
2020-12-05 15:15:36 +0100

    Refactoring

--- Conquer/src/org/jel/game/plugins/ResourceHook.java
+++ Conquer/src/org/jel/game/plugins/ResourceHook.java
@@ -4,9 +4,18 @@ import java.util.List;
 import org.jel.game.data.City;
 import org.jel.game.data.Clan;
 
+/**
+ * Called after resources were produced in a city.
+ */
 @FunctionalInterface
 public interface ResourceHook {
 
+	/**
+	 * 
+	 * @param city       The city
+	 * @param statistics The statistics: &gt; 0 means more was produced
+	 * @param clan       The clan of the city.
+	 */
 	void analyzeStats(final City city, final List<Double> statistics, Clan clan);
 
 }
