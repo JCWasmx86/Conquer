ee2f24e238572c023ba3d3e087ad5f24d9d0dd98
JCWasmx86 <JCWasmx86@t-online.de>
2020-12-05 15:03:28 +0100

    Added documentation

--- Conquer/src/org/jel/game/plugins/MoneyHook.java
+++ Conquer/src/org/jel/game/plugins/MoneyHook.java
@@ -5,8 +5,14 @@ import java.util.List;
 import org.jel.game.data.City;
 import org.jel.game.data.Clan;
 
+/**
+ * Callback that is called when all cities of a clan produced money in a round
+ */
 @FunctionalInterface
 public interface MoneyHook {
-	
+	/**
+	 * @param cities All cities of the clan
+	 * @param clan   The clan
+	 */
 	void moneyPaid(List<City> cities, Clan clan);
 }
