5ad87713f717ac61206c49a3c20c218837229367
JCWasmx86 <JCWasmx86@t-online.de>
2020-12-05 14:55:18 +0100

    Documentation

--- Conquer/src/org/jel/game/plugins/AttackHook.java
+++ Conquer/src/org/jel/game/plugins/AttackHook.java
@@ -3,8 +3,28 @@ package org.jel.game.plugins;
 import org.jel.game.data.AttackResult;
 import org.jel.game.data.City;
 
+/**
+ * Called when before and after an attack was executed.
+ */
 public interface AttackHook {
+	/**
+	 * Called after the attack
+	 * 
+	 * @param src               Origin of the attack
+	 * @param destination       Attacked city
+	 * @param survivingSoldiers Number of soldiers that survived in the attacked
+	 *                          city
+	 * @param result            Result of the attack
+	 */
 	void after(City src, City destination, long survivingSoldiers, AttackResult result);
 
+	/**
+	 * Called before the attack
+	 * 
+	 * @param src                   Origin of the attack
+	 * @param destination           Attacked city
+	 * @param numberOfSoldiersMoved Number of soldiers that attack the destination
+	 *                              city.
+	 */
 	void before(City src, City destination, long numberOfSoldiersMoved);
 }
--- Conquer/src/org/jel/game/plugins/Context.java
+++ Conquer/src/org/jel/game/plugins/Context.java
@@ -6,21 +6,38 @@ import org.jel.game.data.Clan;
 import org.jel.game.data.EventList;
 import org.jel.game.messages.Message;
 
+/**
+ * A collection of data passed to plugins.
+ */
 public final class Context {
 	private final EventList events;
 	private final List<String> clanNames;
 	private final List<Clan> clans;
 
-	public Context(final EventList events2, final List<String> clanNames, final List<Clan> clans) {
-		this.events = events2;
+	/**
+	 * Shouldn't be used from the outside.
+	 */
+	public Context(final EventList events, final List<String> clanNames, final List<Clan> clans) {
+		this.events = events;
 		this.clanNames = clanNames;
 		this.clans = clans;
 	}
 
+	/**
+	 * Add an event to the eventlist notifying all handlers
+	 * 
+	 * @param message The message to add
+	 */
 	public void appendToEventList(Message message) {
 		this.events.add(message);
 	}
 
+	/**
+	 * Get a reference to a clan
+	 * 
+	 * @param index The index
+	 * @return The clan at the specified index.
+	 */
 	public Clan getClan(int index) {
 		return this.clans.get(index);
 	}
