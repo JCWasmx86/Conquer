1f44ffc858f6b08a8acac1b61fda00591e715422
JCWasmx86 <JCWasmx86@t-online.de>
2020-12-14 11:13:47 +0100

    Started i18n in Conquer

--- Conquer/src/org/jel/game/data/Resource.java
+++ Conquer/src/org/jel/game/data/Resource.java
@@ -1,12 +1,18 @@
 package org.jel.game.data;
 
+import org.jel.game.Messages;
+
 /**
  * Enumerates all available resources.
  */
 public enum Resource {
-	WHEAT(0, "wheat.png", "Wheat"), FISH(1, "fish.png", "Fish"), WOOD(2, "wood.png", "Wood"),
-	COAL(3, "coal.png", "Coal"), MEAT(4, "meat.png", "Meat"), IRON(5, "iron.png", "Iron"),
-	TEXTILE(6, "textile.png", "Textile"), LEATHER(7, "leather.png", "Leather"), STONE(8, "stone.png", "Stone");
+	WHEAT(0, "wheat.png", Messages.getString("Resource.wheat")), FISH(1, "fish.png", Messages.getString("Resource.fish")), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+	WOOD(2, "wood.png", Messages.getString("Resource.wood")), //$NON-NLS-1$ //$NON-NLS-2$
+	COAL(3, "coal.png", Messages.getString("Resource.coal")), MEAT(4, "meat.png", Messages.getString("Resource.meat")), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+	IRON(5, "iron.png", Messages.getString("Resource.iron")), //$NON-NLS-1$ //$NON-NLS-2$
+	TEXTILE(6, "textile.png", Messages.getString("Resource.textile")), //$NON-NLS-1$ //$NON-NLS-2$
+	LEATHER(7, "leather.png", Messages.getString("Resource.leather")), //$NON-NLS-1$ //$NON-NLS-2$
+	STONE(8, "stone.png", Messages.getString("Resource.stone")); //$NON-NLS-1$ //$NON-NLS-2$
 
 	private String image;
 	private int index;
