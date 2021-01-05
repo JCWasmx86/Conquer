74572435630b4772381a6b5d9d7b293674b99223
JCWasmx86 <JCWasmx86@t-online.de>
2021-01-05 11:05:53 +0100

    Removed code

--- Conquer/src/org/jel/game/data/XMLReader.java
+++ Conquer/src/org/jel/game/data/XMLReader.java
@@ -1,8 +1,6 @@
 package org.jel.game.data;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
