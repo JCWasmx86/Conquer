ad4437072f5a3ed93c4aa047c946e44212ffa33c
JCWasmx86 <JCWasmx86@t-online.de>
2021-01-09 15:07:56 +0100

    Added other classpaths

--- Launcher/src/jvm.c
+++ Launcher/src/jvm.c
@@ -12,7 +12,8 @@
 void runJVM(Configuration configuration) {
 	char *classpath = generateClasspath(configuration);
 	JavaVMOption *jvmoptions = calloc(
-		configuration->numOptions + NUM_PREDEFINED_ARGS, sizeof(JavaVMOption));
+			configuration->numOptions + NUM_PREDEFINED_ARGS,
+			sizeof(JavaVMOption));
 	assert(jvmoptions);
 	// Just free the first optionstring.
 	jvmoptions[0].optionString = classpath;
@@ -20,15 +21,14 @@ void runJVM(Configuration configuration) {
 	jvmoptions[2].optionString = "-XX:+ShowCodeDetailsInExceptionMessages";
 	for (size_t i = 0; i < configuration->numOptions; i++)
 		jvmoptions[NUM_PREDEFINED_ARGS + i].optionString =
-			configuration->options[i];
-	JavaVMInitArgs vmArgs = {JNI_VERSION_10,
-							 configuration->numOptions + NUM_PREDEFINED_ARGS,
-							 jvmoptions, 1};
+				configuration->options[i];
+	JavaVMInitArgs vmArgs = { JNI_VERSION_10, configuration->numOptions
+			+ NUM_PREDEFINED_ARGS, jvmoptions, 1 };
 	JavaVM *jvm;
 	JNIEnv *env = NULL;
 	void *handle = loadJavaLibrary(configuration);
 	createJVM func = getHandleToFunction(handle);
-	jint status = func(&jvm, (void **)&env, &vmArgs);
+	jint status = func(&jvm, (void**) &env, &vmArgs);
 	if (status != JNI_OK) {
 		fprintf(stderr, "Couldn't create JVM: %d\n", status);
 		goto cleanup;
@@ -38,25 +38,24 @@ void runJVM(Configuration configuration) {
 	jclass stringClass = (*env)->FindClass(env, "java/lang/String");
 	assert(stringClass);
 	jmethodID mainMethod = (*env)->GetStaticMethodID(env, introClass, "main",
-													 "([Ljava/lang/String;)V");
+			"([Ljava/lang/String;)V");
 	jobjectArray arr = (*env)->NewObjectArray(env, 0, stringClass, NULL);
 	(*env)->CallStaticVoidMethod(env, introClass, mainMethod, arr);
 	if ((*env)->ExceptionOccurred(env)) {
 		(*env)->ExceptionDescribe(env);
 	}
 	(*jvm)->DestroyJavaVM(jvm);
-cleanup:
-	free(jvmoptions[0].optionString);
+	cleanup: free(jvmoptions[0].optionString);
 	free(jvmoptions);
 }
-char *generateClasspath(Configuration configuration) {
+char* generateClasspath(Configuration configuration) {
 	char *ret = calloc(1024 * 1024 * 16, 1);
 	assert(ret);
 	strcat(ret, "-Djava.class.path=");
 	char *c = "/";
 #ifndef _WIN32
 	strcat(ret,
-		   "/usr/share/java/Conquer.jar:/usr/share/java/Conquer_frontend.jar:");
+			"/usr/share/java/Conquer.jar:/usr/share/java/Conquer_frontend.jar:");
 #else
 #ifdef UNICODE
 #error UNICODE has to be undefined!
@@ -97,6 +96,41 @@ char *generateClasspath(Configuration configuration) {
 		}
 		closedir(dir);
 	}
+#ifndef _WIN32
+	dir = opendir("/usr/share/java/conquer/plugins");
+	if (dir != NULL) {
+		struct dirent *ent;
+		while ((ent = readdir(dir)) != NULL) {
+			if (ent->d_type == DT_REG) {
+				char *name = ent->d_name;
+				size_t ll = strlen(name);
+				if (ll >= 4 && memcmp(&name[ll - 4], ".jar", 4) == 0) {
+					strcat(ret, libs);
+					strcat(ret, name);
+					strcat(ret, SEP);
+				}
+			}
+		}
+	}
+	dir = opendir("/usr/share/java/conquer/strategies");
+	if (dir != NULL) {
+		struct dirent *ent;
+		while ((ent = readdir(dir)) != NULL) {
+			if (ent->d_type == DT_REG) {
+				char *name = ent->d_name;
+				size_t ll = strlen(name);
+				if (ll >= 4 && memcmp(&name[ll - 4], ".jar", 4) == 0) {
+					strcat(ret, libs);
+					strcat(ret, name);
+					strcat(ret, SEP);
+				}
+			}
+		}
+	}
+	strcat(ret,"/usr/share/conquer/music");
+	strcat(ret,"/usr/share/conquer/sounds");
+	strcat(ret,"/usr/share/conquer/images");
+#endif
 	strcat(ret, libs);
 	strcat(ret, "music");
 	strcat(ret, SEP);
