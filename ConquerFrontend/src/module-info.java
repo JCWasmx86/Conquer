b672ef0f32c9088e0c60f248883b8b772941fbfe
JCWasmx86 <JCWasmx86@t-online.de>
2021-01-13 20:45:36 +0100

    Removed unused code

--- ConquerFrontend/src/module-info.java
+++ ConquerFrontend/src/module-info.java
@@ -1,10 +1,4 @@
 module org.jel.frontend {
 	requires java.desktop;
 	requires org.jel.game;
-	/*requires vorbisspi;
-	requires mp3spi;
-	uses javazoom.spi.mpeg.sampled.file.MpegAudioFileReader;
-	uses javazoom.spi.mpeg.sampled.convert.MpegFormatConversionProvider;
-	uses javazoom.spi.vorbis.sampled.file.VorbisAudioFileReader;
-	uses javazoom.spi.vorbis.sampled.convert.VorbisFormatConversionProvider;*/
 }
