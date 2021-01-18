fd48f9515f2bb0e30f642ae309ad3986540696f2
JCWasmx86 <JCWasmx86@t-online.de>
2021-01-18 21:08:52 +0100

    Added documentation for InstalledScenario

--- Conquer/src/org/jel/game/data/InstalledScenario.java
+++ Conquer/src/org/jel/game/data/InstalledScenario.java
@@ -1,7 +1,9 @@
 package org.jel.game.data;
 
 /**
- * Represents a scenario.
+ * Represents a scenario.<br>
+ * {@code name} is the name of the scenario, {@code file} is the corresponding
+ * file. {@code thumbnail} is an optional picture.
  */
 public final record InstalledScenario(String name, String file, String thumbnail) {
 }
