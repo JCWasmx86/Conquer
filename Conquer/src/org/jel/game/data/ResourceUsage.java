d20e06853d33b966471efd6ef7d56e5ea20f63e6
JCWasmx86 <JCWasmx86@t-online.de>
2021-01-18 21:19:58 +0100

    Added documentation for ResourceUsage

--- Conquer/src/org/jel/game/data/ResourceUsage.java
+++ Conquer/src/org/jel/game/data/ResourceUsage.java
@@ -1,10 +1,39 @@
 package org.jel.game.data;
 
+/**
+ * A class describing the usage of each resource.
+ */
 public class ResourceUsage {
 	private final double[] personUsed;
 	private final double[] soldierUsed;
 	private final double coinsPerRoundPerPerson;
 
+	/**
+	 * The {@code stats}-array has this layout:
+	 * 
+	 * <pre>
+	 * [0][1]
+	 * [0][1]
+	 * [0][1]
+	 * [0][1]
+	 * [0][1]
+	 * [0][1]
+	 * [0][1]
+	 * [0][1]
+	 * [0][1]
+	 * </pre>
+	 * 
+	 * Every row in this array is for one resource. The index is obtained by calling
+	 * {@link Resource#getIndex()}. Each subarray has two entries.
+	 * {@code subarray[0]} is the use of the resource per person,
+	 * {@code subarray[1]} is the usage of the resource per soldier.
+	 * 
+	 * No value may be negative, null, NaN or infinite, otherwise an
+	 * {@code IllegalArgumentException} will be thrown.
+	 * 
+	 * @param stats                  Statistics, may not be {@code null}.
+	 * @param coinsPerRoundPerPerson
+	 */
 	public ResourceUsage(final double[][] stats, final double coinsPerRoundPerPerson) {
 		if (stats == null) {
 			throw new IllegalArgumentException("stats==null");
@@ -39,18 +68,42 @@ public class ResourceUsage {
 		}
 	}
 
+	/**
+	 * Returns the amount of resources for every resource, that every person uses
+	 * per round.
+	 * 
+	 * @return Amount of resources, used by every person.
+	 */
 	public double[] personUsage() {
 		return this.personUsed;
 	}
 
+	/**
+	 * Returns the amount of resources for every resource, that every soldier uses
+	 * per round.
+	 * 
+	 * @return Amount of resources, used by every soldier.
+	 */
 	public double[] soldierUsage() {
 		return this.soldierUsed;
 	}
 
+	/**
+	 * Returns the subarray, as described at
+	 * {@link ResourceUsage#ResourceUsage(double[][], double)}.
+	 * 
+	 * @param idx The index.
+	 * @return Subarray, but just a copy.
+	 */
 	public double[] get(final int idx) {
 		return new double[] { this.personUsed[idx], this.soldierUsed[idx] };
 	}
 
+	/**
+	 * Gives the number of coins every person produces in each round.
+	 * 
+	 * @return Number of coins.
+	 */
 	public double getCoinsPerRoundPerPerson() {
 		return this.coinsPerRoundPerPerson;
 	}
