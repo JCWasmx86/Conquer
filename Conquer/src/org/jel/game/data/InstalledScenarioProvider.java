f48bebe59378260a5a8f92fb68477feaa1c0e612
JCWasmx86 <JCWasmx86@t-online.de>
2021-01-18 21:13:26 +0100

    Added documentation for InstalledScenarioProvider.

--- Conquer/src/org/jel/game/data/InstalledScenarioProvider.java
+++ Conquer/src/org/jel/game/data/InstalledScenarioProvider.java
@@ -2,6 +2,14 @@ package org.jel.game.data;
 
 import java.util.List;
 
+/**
+ * An interface for SPI, that just searches for scenarios.
+ */
 public interface InstalledScenarioProvider {
+	/**
+	 * Find installed scenarios.
+	 * 
+	 * @return All found scenarios. May not be {@code null}.
+	 */
 	List<InstalledScenario> getScenarios();
 }
