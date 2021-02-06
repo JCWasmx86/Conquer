767293960e868dff25e95e087842597fa5206ae2
JCWasmx86 <JCWasmx86@t-online.de>
2021-02-06 16:42:45 +0100

    Removed public from VersionTest

--- Conquer/src/test/java/org/jel/game/data/VersionTest.java
+++ Conquer/src/test/java/org/jel/game/data/VersionTest.java
@@ -2,21 +2,21 @@ package org.jel.game.data;
 
 import org.junit.jupiter.api.Test;
 
-public class VersionTest {
+class VersionTest {
 	@Test
-	public void testComparisonEq() {
+	void testComparisonEq() {
 		final var v = new Version(1, 0, 0);
 		assert v.compareTo(new Version(1, 0, 0)) == 0;
 	}
 
 	@Test
-	public void testComparisonGt() {
+	void testComparisonGt() {
 		final var v = new Version(2, 0, 0);
 		assert v.compareTo(new Version(1, 2, 0)) > 0;
 	}
 
 	@Test
-	public void testComparisonLt() {
+	void testComparisonLt() {
 		final var v = new Version(1, 0, 0);
 		assert v.compareTo(new Version(1, 2, 0)) < 0;
 	}
