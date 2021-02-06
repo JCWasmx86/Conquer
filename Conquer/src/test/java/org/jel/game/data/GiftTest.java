364d18afddc66fa636e5bd915e117932a35ec37b
JCWasmx86 <JCWasmx86@t-online.de>
2021-02-06 16:42:05 +0100

    Removed public from GiftTest

--- Conquer/src/test/java/org/jel/game/data/GiftTest.java
+++ Conquer/src/test/java/org/jel/game/data/GiftTest.java
@@ -5,61 +5,61 @@ import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class GiftTest {
+class GiftTest {
 
 	@Test
-	public void testNegativeCoins() {
-		Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+	void testNegativeCoins() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
 			new Gift(null, -1);
 		});
 	}
 
 	@Test
-	public void testNegativeResources() {
-		Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+	void testNegativeResources() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
 			new Gift(List.of(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0), 50);
 		});
 	}
 
 	@Test
-	public void testNotEnoughResources() {
-		Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+	void testNotEnoughResources() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
 			new Gift(List.of(), 50);
 		});
 	}
 
 	@Test
-	public void testNullResources() {
-		Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+	void testNullResources() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
 			new Gift(null, 50);
 		});
 	}
 
 	@Test
-	public void testNan() {
-		Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+	void testNan() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
 			new Gift(List.of(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, Double.NaN), 50);
 		});
 	}
 
 	@Test
-	public void testNegative() {
-		Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+	void testNegative() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
 			new Gift(List.of(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0), 50);
 		});
 	}
 
 	@Test
-	public void testNegativeInfinity() {
-		Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+	void testNegativeInfinity() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
 			new Gift(List.of(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, Double.NEGATIVE_INFINITY), 50);
 			new Gift(List.of(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, Double.POSITIVE_INFINITY), 50);
 		});
 	}
 
 	@Test
-	public void testPositiveInfinity() {
-		Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+	void testPositiveInfinity() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
 			new Gift(List.of(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, Double.POSITIVE_INFINITY), 50);
 		});
 	}
