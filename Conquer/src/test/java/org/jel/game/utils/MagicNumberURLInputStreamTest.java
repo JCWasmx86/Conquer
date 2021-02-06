4c2f019d151f9df3c177bbf92392c9d7d512c4a6
JCWasmx86 <JCWasmx86@t-online.de>
2021-02-06 16:35:24 +0100

    Improved unit test

--- Conquer/src/test/java/org/jel/game/utils/MagicNumberURLInputStreamTest.java
+++ Conquer/src/test/java/org/jel/game/utils/MagicNumberURLInputStreamTest.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -19,14 +21,29 @@ class MagicNumberURLInputStreamTest {
 
 	@Test
 	void testReadAfterMagicNumber() throws IOException {
-		final var input = new MagicNumberURLInputStream(new URL("https://www.example.com"));
-		final var N = 20;
-		final var magic = input.getMagicNumber(N);
-		for (var i = 0; i < N; i++) {
-			final var cmp = input.read();
-			Assertions.assertEquals(magic[i], cmp);
-			System.out.println(cmp + "//" + magic[i] + "//" + ((char) cmp) + "//" + ((char) magic[i]));
+		try (final var input = new MagicNumberURLInputStream(new URL("https://www.example.com"))) {
+			final var N = 20;
+			final var magic = input.getMagicNumber(N);
+			for (var i = 0; i < N; i++) {
+				final var cmp = input.read();
+				Assertions.assertEquals(magic[i], cmp);
+				System.out.println(cmp + "//" + magic[i] + "//" + ((char) cmp) + "//" + ((char) magic[i]));
+			}
 		}
+	}
+
+	@Test
+	void readFromFile() throws IOException {
+		final var text = "abcdefgh";
+		final var other = "foobarbaz";
+		Files.write(Paths.get("foo.txt"), (text + other).getBytes());
+		try (final var input = new MagicNumberURLInputStream(new File("foo.txt"))) {
+			final var magic = input.getMagicNumber(text.length());
+			Assertions.assertArrayEquals(text.getBytes(), magic);
+			final var bytes = input.readAllBytes();
+			Assertions.assertArrayEquals((text + other).getBytes(), bytes);
+		}
+		Files.delete(Paths.get("foo.txt"));
 	}
 
 	@Test
