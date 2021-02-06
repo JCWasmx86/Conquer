ee9798f9fe04810ae371bb22cd29f09988c4ddbb
JCWasmx86 <JCWasmx86@t-online.de>
2021-02-06 16:28:03 +0100

    Added more constructors and Javadocs

--- Conquer/src/main/java/org/jel/game/utils/MagicNumberURLInputStream.java
+++ Conquer/src/main/java/org/jel/game/utils/MagicNumberURLInputStream.java
@@ -1,11 +1,18 @@
 package org.jel.game.utils;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 
 import org.jel.game.data.MagicNumberInputStream;
 
+/**
+ * An utility class allowing for lazily loading a magic number from a (remote)
+ * URL.
+ */
 public class MagicNumberURLInputStream extends MagicNumberInputStream {
 
 	private URL url;
@@ -13,11 +20,45 @@ public class MagicNumberURLInputStream extends MagicNumberInputStream {
 	private byte[] bytes;
 	private int cnter = 0;
 
+	/**
+	 * Construct a new {@code MagicNumberURLInputStream}.
+	 * 
+	 * @param url The URL to read from. May not be {@code null}, otherwise an
+	 *            {@link IllegalArgumentException} will be thrown.
+	 */
 	public MagicNumberURLInputStream(final URL url) {
 		if (url == null) {
 			throw new IllegalArgumentException("url==null");
 		}
 		this.url = url;
+	}
+
+	/**
+	 * Construct a new {@code MagicNumberURLInputStream}.
+	 * 
+	 * @param file The file to read from. May not be {@code null}, otherwise an
+	 *             {@link IllegalArgumentException} will be thrown.
+	 * @throws MalformedURLException If {@link URI#toURL()} fails.
+	 */
+	public MagicNumberURLInputStream(final File file) throws MalformedURLException {
+		if (file == null) {
+			throw new IllegalArgumentException("file==null");
+		}
+		this.url = file.toURI().toURL();
+	}
+
+	/**
+	 * Construct a new {@code MagicNumberURLInputStream}.
+	 * 
+	 * @param uri The URI to read from. May not be {@code null}, otherwise an
+	 *            {@link IllegalArgumentException} will be thrown.
+	 * @throws MalformedURLException If {@link URI#toURL()} fails.
+	 */
+	public MagicNumberURLInputStream(final URI uri) throws MalformedURLException {
+		if (uri == null) {
+			throw new IllegalArgumentException("uri==null");
+		}
+		this.url = uri.toURL();
 	}
 
 	@Override
