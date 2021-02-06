9478addfdf0c90e8aade7adad063fda761fa8267
JCWasmx86 <JCWasmx86@t-online.de>
2021-02-06 16:42:26 +0100

    Removed public from ResourceUsageTest

--- Conquer/src/test/java/org/jel/game/data/ResourceUsageTest.java
+++ Conquer/src/test/java/org/jel/game/data/ResourceUsageTest.java
@@ -3,84 +3,84 @@ package org.jel.game.data;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class ResourceUsageTest {
+class ResourceUsageTest {
 	@Test
-	public void testStatsIsNull() {
-		Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+	void testStatsIsNull() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
 			new ResourceUsage(null, 0);
 		});
 	}
 
 	@Test
-	public void testStatsIsTooShort() {
-		Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+	void testStatsIsTooShort() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
 			new ResourceUsage(new double[0][0], 0);
 		});
 	}
 
 	@Test
-	public void testStatsArrayHasBadValue() {
-		Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+	void testStatsArrayHasBadValue() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
 			new ResourceUsage(new double[][] { null }, 0.0);
 		});
 	}
 
 	@Test
-	public void testStatsArrayHasBadValue2() {
-		Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+	void testStatsArrayHasBadValue2() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
 			new ResourceUsage(new double[][] { {}, {}, {}, {}, {}, {}, {}, {}, {} }, 0.0);
 		});
 	}
 
 	@Test
-	public void testStatsGood() {
+	void testStatsGood() {
 		new ResourceUsage(new double[][] { { 1, 0 }, { 1, 0 }, { 1, 0 }, { 1, 0 }, { 1, 0 }, { 1, 0 }, { 1, 0 },
 				{ 1, 0 }, { 1, 0 } }, 0.0);
 	}
 
 	@Test
-	public void testStatsSubarrayContainsBadValue() {
-		Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+	void testStatsSubarrayContainsBadValue() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
 			new ResourceUsage(new double[][] { { -1, 0 }, { 1, 0 }, { 1, 0 }, { 1, 0 }, { 1, 0 }, { 1, 0 }, { 1, 0 },
 					{ 1, 0 }, { 1, 0 } }, 0.0);
 		});
 	}
 
 	@Test
-	public void testStatsSubarrayContainsBadValue2() {
-		Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+	void testStatsSubarrayContainsBadValue2() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
 			new ResourceUsage(new double[][] { { Double.NaN, 0 }, { 1, 0 }, { 1, 0 }, { 1, 0 }, { 1, 0 }, { 1, 0 },
 					{ 1, 0 }, { 1, 0 }, { 1, 0 } }, 0.0);
 		});
 	}
 
 	@Test
-	public void testStatsSubarrayContainsBadValue3() {
-		Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+	void testStatsSubarrayContainsBadValue3() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
 			new ResourceUsage(new double[][] { { Double.POSITIVE_INFINITY, 0 }, { 1, 0 }, { 1, 0 }, { 1, 0 }, { 1, 0 },
 					{ 1, 0 }, { 1, 0 }, { 1, 0 }, { 1, 0 } }, 0.0);
 		});
 	}
 
 	@Test
-	public void testCoinsNegative() {
-		Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+	void testCoinsNegative() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
 			new ResourceUsage(new double[][] { { Double.POSITIVE_INFINITY, 0 }, { 1, 0 }, { 1, 0 }, { 1, 0 }, { 1, 0 },
 					{ 1, 0 }, { 1, 0 }, { 1, 0 }, { 1, 0 } }, -1);
 		});
 	}
 
 	@Test
-	public void testCoinsNan() {
-		Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+	void testCoinsNan() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
 			new ResourceUsage(new double[][] { { Double.POSITIVE_INFINITY, 0 }, { 1, 0 }, { 1, 0 }, { 1, 0 }, { 1, 0 },
 					{ 1, 0 }, { 1, 0 }, { 1, 0 }, { 1, 0 } }, Double.NaN);
 		});
 	}
 
 	@Test
-	public void testCoinsInfinity() {
-		Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+	void testCoinsInfinity() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
 			new ResourceUsage(new double[][] { { Double.POSITIVE_INFINITY, 0 }, { 1, 0 }, { 1, 0 }, { 1, 0 }, { 1, 0 },
 					{ 1, 0 }, { 1, 0 }, { 1, 0 }, { 1, 0 } }, Double.POSITIVE_INFINITY);
 		});
