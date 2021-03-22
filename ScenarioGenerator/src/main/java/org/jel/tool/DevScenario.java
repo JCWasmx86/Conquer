8571018297fb857e793709b7396d275cf0b18c47
JCWasmx86 <JCWasmx86@t-online.de>
2021-03-22 19:05:18 +0100

    Added DevScenario

--- ScenarioGenerator/src/main/java/org/jel/tool/DevScenario.java
+++ ScenarioGenerator/src/main/java/org/jel/tool/DevScenario.java
@@ -1,14 +1,15 @@
 package org.jel.tool;
 
 import java.awt.Color;
-import java.util.ArrayList;
+import java.util.List;
 
 public class DevScenario {
 	public static void main(final String[] args) {
 		new DataFile().setBackground("devBackground.png").addPlayer(1000, "Player", new Color(50, 50, 50), 0)
 				.addPlayer(1000, "CPU0", new Color(255, 0, 0), 1).addPlayer(1000, "CPU1", new Color(0, 255, 0), 2)
 				.addPlayer(1000, "CPU2", new Color(0, 0, 255), 3).addCity(new City(1.01, "dev/player.png", "Player0",
-				0, 5000, 500, 20, 20, 500, 1.03, new ArrayList<>()))
+				0, 5000, 500, 20, 20, 500, 1.03, List.of(1.01, 1.01, 1.01, 1.01, 1.01, 1.01, 1.01, 1.01,
+				1.01)))
 				.dump("dev.data");
 	}
 }
