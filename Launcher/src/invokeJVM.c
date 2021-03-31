d635ac33bfa6a72f756974f553775ebc28695b84
JCWasmx86 <JCWasmx86@t-online.de>
2021-03-31 19:53:59 +0200

    Minor addition

--- Launcher/src/invokeJVM.c
+++ Launcher/src/invokeJVM.c
@@ -30,6 +30,9 @@ void launcher_invokeJVM(char **options, int numOptions, char *directory,
 		goto cleanup;
 	}
 	jclass introClass = (*env)->FindClass(env, "conquer/gui/Intro");
+	if((*env)->ExceptionOccurred(env)) {
+		(*env)->ExceptionDescribe(env);
+	}
 	assert(introClass);
 	jclass stringClass = (*env)->FindClass(env, "java/lang/String");
 	assert(stringClass);
