f914301b4b70bdc2a359d6b851499a1e4d0024d7
JCWasmx86 <JCWasmx86@t-online.de>
2021-04-05 19:10:53 +0200

    Removed unused code

--- ConquerFrontend/src/main/java/conquer/gui/ErrorReporterUtils.java
+++ ConquerFrontend/src/main/java/conquer/gui/ErrorReporterUtils.java
@@ -2,14 +2,9 @@ package conquer.gui;
 
 import conquer.data.Shared;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.util.ServiceLoader;
-import java.util.ServiceLoader.Provider;
 
 public final class ErrorReporterUtils {
 	private ErrorReporterUtils() {
@@ -55,56 +50,5 @@ public final class ErrorReporterUtils {
 			}
 		});
 		return sb.toString();
-	}
-
-	// Returns the errorlog filename.
-	public static String writeErrorLog(final Throwable t) {
-		final var reportsDir = new File(Shared.BASE_DIRECTORY, "reports");
-		reportsDir.mkdirs();
-		final var logfileName = "log_" + Long.toHexString(System.nanoTime()) + ".report";
-		final var logfile = new File(reportsDir, logfileName);
-		try (final var bw = new BufferedWriter(new FileWriter(logfile));
-			 final var sw = new StringWriter();
-			 final var pw = new PrintWriter(sw)) {
-			bw.write("Crash log\n");
-			t.printStackTrace(pw);
-			bw.write("Stacktrace\n");
-			bw.write(sw.toString());
-			bw.write("\n");
-			bw.write("System properties\n");
-			System.getProperties().forEach((key, value) -> {
-				try {
-					bw.write(key + "=" + value + "\n");
-				} catch (final IOException e) {
-					Shared.LOGGER.exception(e);
-				}
-			});
-			bw.write("Environment variables\n");
-			System.getenv().forEach((key, value) -> {
-				try {
-					bw.write(key + "=" + value + "\n");
-				} catch (final IOException e) {
-					Shared.LOGGER.exception(e);
-				}
-			});
-			bw.write("Services\n\n");
-			ErrorReporterUtils.class.getModule().getDescriptor().uses().forEach(a -> {
-				try {
-					final var clazz = Class.forName(a);
-					ServiceLoader.load(clazz).stream().map(Provider::get).map(Object::getClass).forEach(b -> {
-						try {
-							bw.write(clazz + " is provided by " + b.toString());
-						} catch (final IOException e) {
-							Shared.LOGGER.exception(e);
-						}
-					});
-				} catch (final ClassNotFoundException cnfe) {
-					Shared.LOGGER.exception(cnfe);
-				}
-			});
-		} catch (final IOException e) {
-			Shared.LOGGER.exception(e);
-		}
-		return logfile.getAbsolutePath();
 	}
 }
