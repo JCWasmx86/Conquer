9989106c696de57afb29287a500ae0bb8880b8c4
JCWasmx86 <JCWasmx86@t-online.de>
2021-04-20 09:09:29 +0200

    Made dependency more explicit

--- auxiliary/install-sdk.sh
+++ auxiliary/install-sdk.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/sh
-type -P mvn &>/dev/null && echo "Found maven" || echo "No maven was found!"
-type -P mvn &>/dev/null || exit
+#!/usr/bin/env bash
+type -P mvn &>/dev/null && echo Found maven || echo No maven was found! Try running \"install-sdk.sh\" manually!
+type -P mvn &>/dev/null || exit 0
 mvn install:install-file -Dfile=/usr/share/conquer/m2/Conquer.jar -DpomFile=/usr/share/conquer/m2/conquer-2.0.0.pom
 mvn install:install-file -Dfile=/usr/share/conquer/m2/Conquer_resources.jar -DpomFile=/usr/share/conquer/m2/conquer_resources-2.0.0.pom
 mvn install:install-file -Dfile=/usr/share/conquer/m2/ConquerFrontendSPI.jar -DpomFile=/usr/share/conquer/m2/conquer_spi-2.0.0.pom
