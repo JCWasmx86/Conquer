312c7de5ae693bcdeb81126e024393cf5e8c71cc
JCWasmx86 <JCWasmx86@t-online.de>
2021-04-20 08:35:32 +0200

    maven is no hard dependency anymore, as it is outdated on debian, and may be installed with e.g. sdkman.

--- auxiliary/install-sdk.sh
+++ auxiliary/install-sdk.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/sh
+type -P mvn &>/dev/null && echo "Found maven" || echo "No maven was found!"
+type -P mvn &>/dev/null || exit
 mvn install:install-file -Dfile=/usr/share/conquer/m2/Conquer.jar -DpomFile=/usr/share/conquer/m2/conquer-2.0.0.pom
 mvn install:install-file -Dfile=/usr/share/conquer/m2/Conquer_resources.jar -DpomFile=/usr/share/conquer/m2/conquer_resources-2.0.0.pom
 mvn install:install-file -Dfile=/usr/share/conquer/m2/ConquerFrontendSPI.jar -DpomFile=/usr/share/conquer/m2/conquer_spi-2.0.0.pom
+echo To install the jars in the local repo, run \"install-sdk.sh\"!
