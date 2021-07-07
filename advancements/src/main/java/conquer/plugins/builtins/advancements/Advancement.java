13d270d8e3ca59c2ed286b3b4b58b3b604db2fe4
JCWasmx86 <JCWasmx86@t-online.de>
2021-07-07 16:27:35 +0200

    Removed files

--- advancements/src/main/java/conquer/plugins/builtins/advancements/Advancement.java
+++
@@ -1,9 +0,0 @@
-package conquer.plugins.builtins.advancements;
-
-import java.util.Optional;
-
-public interface Advancement {
-	default Optional<Advancement> getDependency() {
-		return Optional.empty();
-	}
-}
